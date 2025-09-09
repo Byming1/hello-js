96ad453d73b9aef1e59927cdc9542d47c7c4ae23
Byming1 <pqm1290@gmail.com>
2025-09-09 21:29:00 +0700

    part4

---
+++ part-4/part4.js
@@ -0,0 +1,32 @@
+console.log("Hello part4");
+
+//arithmetic operators
+const a = 10;
+const b = 20, c = 30;
+console.log(a + b);
+console.log(b * c);
+console.log(a - b);
+console.log(c / a);
+console.log(b % c);
+console.log(a ** 2); // a squared
+console.log(b ** 3); // b cubed
+
+let d = 5;
+d = a + (b - c);
+console.log(d);
+
+//comparison operators
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a == b);
+console.log(a != b);
+console.log(a === b); //strict equality
+console.log(a !== b); //strict inequality
+
+//logical operators
+console.log(a < b && b < c); //and
+console.log(a < b || b > c); //or
+console.log(!(a < b)); //not
+
