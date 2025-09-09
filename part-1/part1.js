035d9d06ebe2094a82b4158bcf9763a734d97211
Byming1 <pqm1290@gmail.com>
2025-09-09 20:38:36 +0700

    part1

---
+++ part-1/part1.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+var name = 'ByMing';
+var name2 = "ByMing2";
+let name3 = 'ByMing3';
+const name4 = 'ByMing4';
+console.log(name);
+console.log(name2);
+console.log(name3);
+console.log(name4);
