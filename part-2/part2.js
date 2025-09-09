97ba911cee8e393f52210c0b84ef53e08d66d272
Byming1 <pqm1290@gmail.com>
2025-09-09 20:51:27 +0700

    part2

---
+++ part-2/part2.js
@@ -0,0 +1,32 @@
+console.log("hello, part2");
+
+//number
+const age = 20;
+const myScore = 99.5;
+
+console.log(age);
+console.log(myScore);
+
+//string
+const myName = "ByMing"; // better
+const myName2 = 'ByMing2';
+const myName3 = `ByMing3`;
+
+console.log(myName);
+console.log(myName2);
+console.log(myName3);
+
+//boolean
+const isMale = true;
+const isFemale = false;
+
+console.log(isMale);
+console.log(isFemale);
+
+//undefined
+let x;
+console.log(x);
+
+//null
+let y = null;
+console.log(y);
